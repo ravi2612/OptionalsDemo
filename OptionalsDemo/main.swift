936712a3d128469fc1049ee1f0e7f7bc4c671f20
ravi <ravinavarro26@gmail.com>
2021-02-09 14:24:20 -0300

    optionals

--- OptionalsDemo/main.swift
+++ OptionalsDemo/main.swift
@@ -1,11 +1,83 @@
-//
-//  main.swift
-//  OptionalsDemo
-//
-//  Created by raji navarro on 09/02/21.
-//
 
-import Foundation
+func forceUnwrapping() {
+    
+    //    myOptional!
+    
+    let myOptional : String?
 
-print("Hello, World!")
+    myOptional = "Angela"
+
+    let text: String = myOptional!
+}
+
+
+func checkForNilValue()  {
+    
+    //   if optional != nil { optional! }
+    
+    
+    let myOptional: String?
+    myOptional = nil
+    
+    if myOptional != nil {
+        let text: String = myOptional!
+    } else {
+        print("myOptional was found to be nil. ")
+    }
+}
+
+func optionalBinding() {
+    
+    //   if let safeOptional = Optional {
+    //      safeOptional
+    //     }
+    
+    let myOptional: String?
+    
+    myOptional = "Angela"
+    
+    if let safeOptional = myOptional {
+        let text: String = safeOptional
+        print(safeOptional)
+        
+    } else {
+        
+        print("myOptional was found to be nil. ")
+    }
+    
+}
+
+func nilCoalescingOperator() {
+    
+    //  optional ?? defaultValue
+    
+    let myOptional: String?
+    
+    myOptional = nil
+    
+    let text: String = myOptional ?? "I am the defaut value"
+    
+    print(text)
+    
+}
+
+struct MyOptional {
+    var property = 123
+    func method() {
+        print("I am the struct's method. ")
+    }
+}
+
+// optional?.property
+let myOptional: MyOptional?
+
+
+myOptional = nil
+
+print(myOptional?.property)
+
+myOptional?.method()
+
+
+
 
